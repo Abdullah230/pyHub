cab769b28ad2191f3d28e19bc88652affc97ae0e
Abdullah Khan <mabdullah.bee15seecs@seecs.edu.pk>
2020-04-13 08:44:30 +0500

    Decided the structure of implementation

---
+++ Pattern_Searching/Aho_Corasick.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Abdullah
+@Email: abdullah.khan230@yahoo.com
+@Github: https://github.com/Abdullah230
+Created on Sun Apr 12 21:48:02 2020
+"""
+#Trees will use methods to use Nodes
+class tree:
+
+    
+    
+# Nodes are just structures
+class Node:
+    
+    
+    
+    
+# f will define where to jump in case of a failure
+# O will define the ouput
