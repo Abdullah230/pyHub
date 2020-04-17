32adf006ddc63afffa5eb5bc0757f42bdae3601d
Abdullah Khan <mabdullah.bee15seecs@seecs.edu.pk>
2020-04-18 02:02:44 +0500

    Made node structure and added tree. Append node function has to be edited

--- Pattern_Searching/Aho_Corasick.py
+++ Pattern_Searching/Aho_Corasick.py
@@ -6,15 +6,47 @@
 Created on Sun Apr 12 21:48:02 2020
 """
 #Trees will use methods to use Nodes
-class tree:
+def get_key(val, dictionary):
+    for key, value in dictionary.items():
+        if value == val:
+            return key
 
+class tree:
+    def __init__(self,tree):
+        self.treeList = []
+        self.nodes = 0
     
-    
+        
+    def append_node(self,word):
+        i = 0
+        j = 0
+        if self.nodes == 0:
+            self.treeList.append(Node(self.nodes+1, word[i]))
+        
+        else:
+            if len(self.treeList[0].children) == 0:
+                self.treeList[0].attach_node(Node(self.nodes+1, letter))
+                
+            else:
+                while  in self.treeList[i].children.values():
+                    i = get_key(letter, self.treeList[i].children)
+                
+        
+
+        
 # Nodes are just structures
 class Node:
     
-    
-    
+    def __init__(self, number, letter):
+        self.children = {}
+        self.parent = 0
+        self.number = number
+        self.letter = letter
+        
+    def attach_node(self,Node):
+        self.children[Node.number] = Node.letter
+        Node.parent = self.number
+        
     
 # f will define where to jump in case of a failure
 # O will define the ouput
