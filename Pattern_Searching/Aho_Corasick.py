9e2c3a4cdd0433577065cf355d39b8940b0f9733
Abdullah Khan <mabdullah.bee15seecs@seecs.edu.pk>
2020-04-28 05:32:12 +0500

    Solved all parents being 0 issue

--- Pattern_Searching/Aho_Corasick.py
+++ Pattern_Searching/Aho_Corasick.py
@@ -15,12 +15,14 @@ class tree:
     def __init__(self):
         self.treeList = []
         self.nodes = 0
+        self.o = []
+        self.alphabets = []
         return
-        
+    
     def append_words(self,words):
-        j = 0
+        k = 0
         for word in words:
-            k = 0
+            j = 0
             for i in range(len(word)):
                 if self.nodes == 0:
                     self.treeList.append(Node(self.nodes, ''))
@@ -28,40 +30,60 @@ class tree:
     
                 if len(self.treeList[0].children) == 0:
                     self.treeList.append(Node(self.nodes, word[i]))
-                    self.treeList[0].attach_node(Node(self.nodes, word[i]))
+                    ph = Node(self.nodes, word[i])
+                    self.treeList[0].attach_node(ph)
                     self.nodes += 1
                     j += 1
                     if i == (len(word) - 1):
-                        self.treeList[j].end.append(k)
+                        self.treeList[j].end.append(k+1)
                     
                 else:
                     if word[i] in self.treeList[j].children.values():
                         j = get_key(word[i], self.treeList[j].children)
                     else:
                         self.treeList.append(Node(self.nodes, word[i]))
-                        self.treeList[j].attach_node(Node(self.nodes, word[i]))
+                        ph = Node(self.nodes, word[i])
+                        self.treeList[j].attach_node(ph)
+                        j = self.nodes
                         self.nodes += 1
-                        j += 1
+                        
                     
                     if i == len(word) - 1:
-                        self.treeList[j].end.append(k)
-            k += 1
+                        self.treeList[j].end.append(k+1)
+            k += 1          
         
-        for i in self.treeList:
-            print(i.children)
-        return
-
+        for i in range(len(self.treeList) - 1):
+            if len(self.treeList[i+1].end) == 0:
+                self.o.append(0)
+            else:
+                self.o.append(self.treeList[i+1].end[0])
+        
+        for word in words:
+            for i in range(len(word)):
+                if word[i] not in self.alphabets:
+                    self.alphabets.append(word[i])
+        
+        for i in range(len(self.treeList)):
+            print(self.treeList[i].parent)
+        
+        return            
+#        for i in range(len(self.treeList) - 1):
+#            temp = []
+#            j = self.treeList[i+1].number
+#            while self.treeList[j].parent != 0:
+#                
+#                
 
         
 # Nodes are just structures
 class Node:
-    
     def __init__(self, number, letter):
         self.children = {}
         self.parent = 0
         self.number = number
         self.letter = letter
         self.end = []
+        self.f = []
         
     def attach_node(self,Node):
         self.children[Node.number] = Node.letter
