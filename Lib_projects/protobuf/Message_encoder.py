9d4238f21ce36aba1f84d017a45ba28ac0317479
Abdullah Khan <mabdullah.bee15seecs@seecs.edu.pk>
2020-07-17 01:20:43 +0500

    Added protobuf python code

--- Lib_projects/protobuf/Message_encoder.py
+++ Lib_projects/protobuf/Message_encoder.py
@@ -1,1 +1,9 @@
-import 
+import sys
+sys.path.insert(0, 'F:/Commits/Python/Lib_projects/protobuf/')
+import test_pb2
+
+message = test_pb2.test_message()
+message.user_number = 234
+message.user_message = "This is a user message"
+message.extra_details.extend(["This is supposed to be a mesage", "This is supposed to be message numner 2"])
+
