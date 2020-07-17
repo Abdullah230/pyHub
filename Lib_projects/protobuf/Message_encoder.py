16f0f7973963f46135c20ceb7ba765a51f82b8ab
Abdullah Khan <mabdullah.bee15seecs@seecs.edu.pk>
2020-07-17 22:59:55 +0500

    Updated protobuf to include message display

--- Lib_projects/protobuf/Message_encoder.py
+++ Lib_projects/protobuf/Message_encoder.py
@@ -7,3 +7,14 @@ message.user_number = 234
 message.user_message = "This is a user message"
 message.extra_details.extend(["This is supposed to be a mesage", "This is supposed to be message numner 2"])
 
+# Encoding message to be sent over the wire
+binary = message.SerializeToString()
+
+print(binary)
+
+
+# Converting it back to message object
+message2 = test_pb2.test_message()
+message2.ParseFromString(binary)
+
+print(message2)
